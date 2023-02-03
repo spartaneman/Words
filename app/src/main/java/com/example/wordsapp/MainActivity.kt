9d563c24153f9f6306d854f198b7b9280dc2ec68
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-02-02 18:09:38 -0800

    Activity and Intent Course Android Studio initial commit

---
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wordsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ *
+ * there are 2 types of intents - implicit and explicit
+ * explicit is highly specific, generally when showing an activity in your own app
+ *
+ * implicit is a bit more abstract - where you tell the system the type of action, such as opening a link
+ * usually involving other apps or web pages or system features.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+
+}
